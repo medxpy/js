77b0a55383e73d7b80cf192238190270b2a7180c
medxpy <62339396+medxpy@users.noreply.github.com>
2020-04-27 01:19:11 +0000

    Mini Linter
    In this project, you will use what you know about iterating over arrays to improve the quality of a paragraph and gather some information about that paragraph.
    
    This is the same type of work that word processing software does. Additionally, you may have heard of linting, a process by which text is evaluated and improved by an application. In this project, you will create a miniature version of a linter using array methods that you have learned

---
+++ main.js
@@ -0,0 +1,46 @@
+let story = 'Last weekend, I took literally the most beautiful bike ride of my life. The route is called "The 9W to Nyack" and it actually stretches all the way from Riverside Park in Manhattan to South Nyack, New Jersey. It\'s really an adventure from beginning to end! It is a 48 mile loop and it basically took me an entire day. I stopped at Riverbank State Park to take some extremely artsy photos. It was a short stop, though, because I had a really long way left to go. After a quick photo op at the very popular Little Red Lighthouse, I began my trek across the George Washington Bridge into New Jersey.  The GW is actually very long - 4,760 feet! I was already very tired by the time I got to the other side.  An hour later, I reached Greenbrook Nature Sanctuary, an extremely beautiful park along the coast of the Hudson.  Something that was very surprising to me was that near the end of the route you actually cross back into New York! At this point, you are very close to the end.';
+
+let overusedWords = ['really', 'very', 'basically'];
+
+let unnecessaryWords = ['extremely', 'literally', 'actually' ];
+
+let storyWords = story.split(' ')
+
+// console.log(storyWords.length)
+
+let betterWords = storyWords.filter(word =>{
+  return !unnecessaryWords.includes(word)
+})
+// console.log(betterWords.length)
+
+let countReally = 0
+let countVery = 0
+let countBasically = 0
+
+for (word of storyWords){
+  if (word === 'really'){
+    countReally += 1
+  }
+  else if (word === 'very'){
+    countVery += 1
+  }
+  else if (word === 'basically'){
+    countBasically += 1
+  }
+}
+// console.log('You use word really: ', countReally)
+// console.log('You use word very: ', countVery)
+// console.log('You use word basically: ', countBasically)
+let sentence = 0
+betterWords.forEach(word => {
+  if (word[word.length-1] === '.' || word[word.length-1] === '!'){
+    sentence += 1
+  }
+})
+console.log('Word count: ' ,storyWords.length)
+console.log('Sentence count: ' ,sentence)
+console.log('Count really: ' ,countReally)
+console.log('Count very: ' ,countVery)
+console.log('Count basically: ' ,countBasically)
+console.log('************')
+console.log(betterWords.join(' '))
